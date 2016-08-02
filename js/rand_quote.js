6adf946831fdae0568a769787973a405e8325bec
bgeraci714 <bgeraci851@hcc.edu>
2016-08-02 19:03:16 -0400

    quote validation

--- js/rand_quote.js
+++ js/rand_quote.js
@@ -35,4 +35,11 @@ $(document).ready(function() {
             }
         });
     });
+    $("#twitter-button").on("click", function(){
+        var STARTING_TEXT = "Random Movie Quotes for Your Enjoyment";
+        if (".message".html() === STARTING_TEXT) {
+            $(".message").hide().html("You need to get a quote before you can tweet it!").fadeIn();
+            $("#twit-btn").attr("href","#")
+        }
+    });
 });
