83a5578b0217a7c8aea8031028dc5814521b7401
bgeraci714 <bgeraci851@hcc.edu>
2016-08-05 00:34:11 -0400

    commenting out initial geolocation

--- js/weatherapp.js
+++ js/weatherapp.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
+    /*
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
             $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
         });
     }
+    */
     $.getJSON("https://freegeoip.net/json/", function(data) {
         $("#data").html("latitudeAPI: " + data.latitude + "<br>longitudeAPI: " + data.longitude);
     });
