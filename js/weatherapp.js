58e012dfe655ed69198b9d394bc05ec16c4cce80
bgeraci714 <bgeraci851@hcc.edu>
2016-08-05 00:16:07 -0400

    testing with basic http vs https

--- js/weatherapp.js
+++ js/weatherapp.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
             $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
         });
     }
-    $.getJSON("http://ipinfo.io", function(data) {
+    $.getJSON("https://ipinfo.io", function(data) {
         console.log(data)
     });
 });
