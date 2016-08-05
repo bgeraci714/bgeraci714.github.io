dbb3a666d6fd05b3951874e6460cb0aabf9fdb07
bgeraci714 <bgeraci851@hcc.edu>
2016-08-04 23:41:07 -0400

    getting started with the weather app

---
+++ js/weatherapp.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    if (navigator.geolocation) {
+ navigator.geolocation.getCurrentPosition(function(position) {
+   $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+ });
+}
+});
