ac46a5cad56e1e46731c13757f4ad8c7309fff0c
bgeraci714 <bgeraci851@hcc.edu>
2016-08-05 09:11:05 -0400

    weather app progress
    have a working prototype of the weather application.

--- js/weatherapp.js
+++ js/weatherapp.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
-    /*
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-        });
-    }
-    */
-    $.getJSON("https://freegeoip.net/json/", function(data) {
-        $("#data").html("latitudeAPI: " + data.latitude + "<br>longitudeAPI: " + data.longitude);
+    var currentZip;
+
+    $.getJSON("https://freegeoip.net/json/", function(locationData) {
+        //console.log(locationData);
+        localWeatherData(locationData);
+
     });
 });
+
+function localWeatherData (locationData) {
+    // API key is 176611c00d609a0919ef1cd4b31da46b
+    var weatherMapURL = "http://api.openweathermap.org/data/2.5/weather?APPID=176611c00d609a0919ef1cd4b31da46b&zip="
+    $.getJSON(weatherMapURL + locationData.zip_code, function(weatherData) {
+        var temp = weatherData.main.temp;
+
+        tempF = kToF(temp);
+
+        $("#data").html("The current weather in " + locationData.city + " is " + weatherData.weather[0].main + ".<br>The current temperature is " + tempF + "&deg;F.");
+    });
+}
+
+function kToF (kelvinTemp) {
+    return Math.round(kelvinTemp * 9 / 5 - 459.67);
+}
