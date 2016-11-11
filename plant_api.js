5c28dff3c81487f38f7c1161e8b023a45645dd1b
Blake Geraci <bgeraci851@hcc.edu>
2016-11-10 22:33:53 -0500

    Uploading Plant API html and css files

---
+++ plant_api.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+  generateDBPanel();
+
+
+  $("#newPlantButton").click(function(e) {
+    e.preventDefault();
+    //console.log($("#plantName").val());
+    var plant = {
+        plant:
+              {
+                name:  $("#plantName").val(),
+                description: $("#plantDesc").val()
+              }
+        };
+
+    //console.log(plant);
+    $.ajax({
+      url: "http://localhost:3000/api/plants/",
+      method: "POST",
+      data: plant,
+      dataType: 'json',
+      success: generateDBPanel
+    });
+  });
+});
+
+function dataToPagePanel(data) {
+  //console.log(data);
+  $("div.demo").html(function() {
+    tableHTML = '<ul class="list-group">';
+    for (resourceItem in data) {
+      //console.log(resourceItem);
+      tableHTML += '<li class="list-group-item">';
+      tableHTML += data[resourceItem].name + ": " + data[resourceItem].description;
+      tableHTML += "</li>"
+    };
+    tableHTML += "</ul>"
+    return tableHTML;
+  });
+}
+
+function generateDBPanel(){
+  $.ajax({
+    url: "http://localhost:3000/api/plants/",
+    method: "GET",
+    success: function(data) {
+      dataToPagePanel(data);
+    },
+    error: function(xhr, status, error) {
+      console.log(status, error);
+    }
+  });
+}
